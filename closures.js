cce4a14ac4b49c7e991a855d5a511552c8c76db7
Dustin <leedu708@gmail.com>
2015-12-17 20:45:21 -0500

    complete assignment

--- closures.js
+++ closures.js
@@ -16,8 +16,10 @@ assignments.one = function(){
   for (var i = 0; i < buttons.length; i++) {
 
     // somehow, i is always the same value
+    // loop has already finished running so i = 4
+    // instead, use the index of the element in buttons
      $(buttons[i]).on('click', function() {
-        $('#clicked-btn').text('You clicked button #' + i);
+        $('#clicked-btn').text('You clicked button #' + buttons.index(this));
      });
   }
 
@@ -42,7 +44,9 @@ assignments.two = function(){
 
                           //So what goes wrong here?
                           setTimeout( (function() {
-                            this.mood = "Happy!";
+                            // this = window
+                            // viking is within the closure, so we can use mood as defined on viking
+                            viking.mood = "Happy!";
 
                             //THIS even runs correctly!
                             //What is UP with this? :(
